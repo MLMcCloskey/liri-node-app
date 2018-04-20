6f9673c576ac21daabbf3a6793c352e17d2bbe2c
Matt McCloskey <matthew.lawrence.mccloskey@gmail.com>
2018-04-20 19:35:34 -0400

    added log

--- liri.js
+++ liri.js
@@ -35,18 +35,36 @@ var runFunction = function(){
                     "\nAlbum: "+response.tracks.items[0].album.name,
                     "\nSample: "+response.tracks.items[0].external_urls.spotify
                 );
+                fs.appendFile("log.txt",
+                  "\nArtist: "+response.tracks.items[0].artists[0].name+
+                  "\nTrack: "+response.tracks.items[0].name+
+                  "\nAlbum: "+response.tracks.items[0].album.name+
+                  "\nSample: "+response.tracks.items[0].external_urls.spotify+"\n",
+                  function(err){
+                    if (err){
+                        return console.log(err)
+                    }
+                }) 
             })
             .catch(function(err) {
                 console.log(err);
             });
-    }
+        }
 
     if (a==="my-tweets"){
         var params = {screen_name: 'Sklooby McDoobie'};
         client.get('statuses/user_timeline', params, function(error, tweets, response) {
         if (!error) {
             for (i=0; i<20; i++){
-                console.log(tweets[i].created_at, '\n', tweets[i].text);
+                console.log(tweets[i].created_at, '\n', tweets[i].text,'\n');
+                fs.appendFile("log.txt",
+                      "\n"+tweets[i].created_at+
+                      "\n"+tweets[i].text+"\n",
+                      function(err){
+                        if (err){
+                            return console.log(err)
+                        }
+                    })
             }
         }
         });
@@ -67,6 +85,19 @@ var runFunction = function(){
                     "\nThe movie is about: " + JSON.parse(body).Plot,
                     "\nThe movie stars: " + JSON.parse(body).Actors,
                 );
+                fs.appendFile("log.txt",
+                    "\nThe movie's title is: " + JSON.parse(body).Title+
+                    "\nThe came out in: " + JSON.parse(body).Title+
+                    "\nThe movie's rating on IMDB is: " + JSON.parse(body).imdbRating+
+                    "\nThe movie's title is: " + JSON.parse(body).Title+
+                    "\nThe movie was produced in: " + JSON.parse(body).Country+
+                    "\nThe movie is about: " + JSON.parse(body).Plot+
+                    "\nThe movie stars: " + JSON.parse(body).Actors+"\n",
+                  function(err){
+                    if (err){
+                        return console.log(err)
+                    }
+                })
             }
         });
     }
